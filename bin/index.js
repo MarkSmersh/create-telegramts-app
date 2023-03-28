fa5508758e4f68a15682a9f286d7127700398f66
Say my name <marksmersh01@gmail.com>
2023-03-28 13:37:43 +0200

    edit config, rm bin when setup

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-// const { execSync } = require("child_process");
-import { execSync } from "child_process"
+const { execSync } = require("child_process");
+const fs = require("fs");
 
 function commandExecute(command) {
     try {
@@ -13,17 +13,21 @@ function commandExecute(command) {
     return true;
 }
 
-const repoName = process.argv[2];
+const repoName = process.argv[2] || ".";
+
 const gitCheckCommand = `git clone --depth 1 https://github.com/MarkSmersh/create-telegramts-app ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm i`;
 
-console.log(`Cloning ${repoName} repository...`);
+console.log(`Cloning into ${repoName} repository...`);
 const checkedOut = commandExecute(gitCheckCommand);
 if (!checkedOut) process.exit(-1);
 
 console.log(`Installing dependecies...`)
 const installedDeps = commandExecute(installDepsCommand);
 if (!installedDeps) process.exit(-1);
+
+console.log(`Removing trash...`)
+fs.rmSync(`./${repoName}/bin`, { force: true, recursive: true });
 
 console.log("Instalation is completed. Use next commands to start:");
 console.log(`cd ${repoName} && npm start`);
